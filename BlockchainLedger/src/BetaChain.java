de3e211047b3625fb5be56f0d3c0019f0169b1e8
Sai <9091157+SaiG18@users.noreply.github.com>
2018-03-11 09:54:38 -0400

    Add files via upload

---
+++ BlockchainLedger/src/BetaChain.java
@@ -0,0 +1,170 @@
+
+import java.security.PublicKey;
+import java.security.Security;
+import java.util.ArrayList;
+//import java.util.Base64;
+import java.util.HashMap;
+//import com.google.gson.GsonBuilder;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.*;
+
+public class BetaChain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>();
+	
+	public static int difficulty = 3;
+	public static float minimumTransaction = 0.1f;
+	public static Wallet walletA;
+	public static Wallet walletB;
+	public static Transaction genesisTransaction;
+
+	public static void main(String[] args) {	
+		//add our blocks to the blockchain ArrayList:
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); //Setup Bouncey castle as a Security Provider
+		
+		//Create wallets:
+		walletA = new Wallet();
+		walletB = new Wallet();		
+		Wallet coinbase = new Wallet();
+		float balance=100;
+		
+		
+		//create genesis transaction, which sends 100 BetaCoin to walletA: 
+		genesisTransaction = new Transaction(coinbase.publicKey, walletA.publicKey, balance, null);
+		genesisTransaction.generateSignature(coinbase.privateKey);	 //manually sign the genesis transaction	
+		genesisTransaction.transactionId = "0"; //manually set the transaction id
+		genesisTransaction.outputs.add(new TransactionOutput(genesisTransaction.reciepient, genesisTransaction.value, genesisTransaction.transactionId)); //manually add the Transactions Output
+		UTXOs.put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0)); //its important to store our first transaction in the UTXOs list.
+		
+		System.out.println("Creating and Mining Genesis block... ");
+		Block genesis = new Block("0");
+		genesis.addTransaction(genesisTransaction);
+		addBlock(genesis);
+		
+		//testing
+		//For demo purpose
+		System.out.println(walletB.publicKey);// This is just for demonstration purposes,in actuality the address to
+											//which the money is being sent to would be known by the user
+		System.out.println();
+		System.out.println("What is the destination address?: ");//copy displayed address
+		Scanner destination=new Scanner(System.in);
+		String AltAddress=destination.nextLine();//this line would be taken out as a scanner would be developed for PublicKey
+		PublicKey address=walletB.publicKey;
+		System.out.println("Your current wallet Balance is: "+balance);
+		System.out.println("How much is to be sent?: ");
+		Scanner amount=new Scanner(System.in);
+		float money=amount.nextFloat();
+		Block block1 = new Block(genesis.hash);
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("\nWalletA is Attempting to send funds ("+money+")  to WalletB...");
+		block1.addTransaction(walletA.sendFunds(address, money));
+		addBlock(block1);//Because of this line a loop can't be used
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("WalletB's balance is: " + walletB.getBalance());
+		
+		//For demo purpose
+		System.out.println(walletB.publicKey);
+		System.out.println();
+		System.out.println("What is the destination address?: ");
+		destination=new Scanner(System.in);
+		AltAddress=destination.nextLine();
+		System.out.println("Your current wallet Balance is: "+walletA.getBalance());
+		System.out.println("How much is to be sent?: ");
+		amount=new Scanner(System.in);
+		money=amount.nextFloat();
+		Block block2 = new Block(block1.hash);
+		System.out.println("\nWalletA is Attempting to send funds ("+money+")  to WalletB...");
+		block2.addTransaction(walletA.sendFunds(address, money));
+		addBlock(block2);
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("WalletB's balance is: " + walletB.getBalance());
+		
+		isChainValid();
+		
+	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		HashMap<String,TransactionOutput> tempUTXOs = new HashMap<String,TransactionOutput>(); //a temporary working list of unspent transactions at a given block state.
+		tempUTXOs.put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0));
+		
+		//loop through betachain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("#Current Hashes not equal");
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("#Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("#This block hasn't been mined");
+				return false;
+			}
+			
+			//loop through betachain transactions:
+			TransactionOutput tempOutput;
+			for(int t=0; t <currentBlock.transactions.size(); t++) {
+				Transaction currentTransaction = currentBlock.transactions.get(t);
+				
+				if(!currentTransaction.verifySignature()) {
+					System.out.println("#Signature on Transaction(" + t + ") is Invalid");
+					return false; 
+				}
+				if(currentTransaction.getInputsValue() != currentTransaction.getOutputsValue()) {
+					System.out.println("#Inputs are note equal to outputs on Transaction(" + t + ")");
+					return false; 
+				}
+				
+				for(TransactionInput input: currentTransaction.inputs) {	
+					tempOutput = tempUTXOs.get(input.transactionOutputId);
+					
+					if(tempOutput == null) {
+						System.out.println("#Referenced input on Transaction(" + t + ") is Missing");
+						return false;
+					}
+					
+					if(input.UTXO.value != tempOutput.value) {
+						System.out.println("#Referenced input Transaction(" + t + ") value is Invalid");
+						return false;
+					}
+					
+					tempUTXOs.remove(input.transactionOutputId);
+				}
+				
+				for(TransactionOutput output: currentTransaction.outputs) {
+					tempUTXOs.put(output.id, output);
+				}
+				
+				if( currentTransaction.outputs.get(0).reciepient != currentTransaction.reciepient) {
+					System.out.println("#Transaction(" + t + ") output reciepient is not who it should be");
+					return false;
+				}
+				if( currentTransaction.outputs.get(1).reciepient != currentTransaction.sender) {
+					System.out.println("#Transaction(" + t + ") output 'change' is not sender.");
+					return false;
+				}
+				
+			}
+			
+		}
+		System.out.println("Blockchain is valid");
+		return true;
+	}
+	
+	public static void addBlock(Block newBlock) {
+		newBlock.mineBlock(difficulty);
+		blockchain.add(newBlock);
+	}
+}
---
+++ BlockchainLedger/src/UI.java
@@ -0,0 +1,14 @@
+import java.security.Security;
+import java.util.ArrayList;
+//import java.util.Base64;
+import java.util.HashMap;
+//import com.google.gson.GsonBuilder;
+import java.util.Map;
+
+public class UI extends BetaChain
+{
+	public static void main(String[] args)
+	{
+		System.out.print(walletA.getBalance());
+	}
+}
